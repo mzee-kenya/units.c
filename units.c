88579cb0750884f0b702f9d3476c7b49c2e47cff
Lenox Okoth <lenox11458@gmail.com>
2025-09-14 19:31:23 +0300

    Update units.c

--- units.c
+++ units.c
@@ -1,1 +1,20 @@
-#
+// 
+/* 
+Name: Okoth Lenox Otieno 
+REG: PA106/G/28809/25
+*/
+#include<stdio.h> // preprocessor  directivees
+
+int main() { 
+     printf ("UNITS FOR SOFTWARE ENGINEERING \n");
+     printf ("Fundamentals of ICT\n");
+     printf("Introduction To Computer System And Organization\n");
+     printf("Mathematics for science\n");
+     printf("Discrete Mathematics\n");
+     printf("Communication skills\n");
+     printf("Introduction to Programming and Problem Solving\n");
+     printf("System Analysis and design");
+     
+     return 0;
+
+}
